501fbd189631ad61310029469f634a0c3c945bda
BettyHoPro <bettyvisualtech@gmail.com>
2021-01-14 22:23:29 +0000

    update the correct  file for timer

--- timer1.js
+++ timer1.js
@@ -1,40 +1,52 @@
 
-// console.log(process.argv.sort((a,b)=>{return a-b}));
-// console.log(process.argv.length-2);
-//let arr=(process.argv).slice(2);
-
-  // for (let i=2; i < process.argv.length-1; i ++ ) {
-  //   //console.log(i);
-  //   //timeout += i;
-  //   if (process.argv[i] === Number) {
-  //     beep=process.argv[i];
-  //     setTimeout(() => {
-  //     process.stdout.write('\x07');
-  //     }, i );
-  //   }
-  //   // }else if( process.argv[i] < 0 || i !== Number){
-  //   //   break;
-  //   // }else if (i < 0 || i !== Number ){
-  //   //    break;
-  //   // }else{
-  //   //   break;
-  //   // }
-  // }
-let arr=process.argv.slice(2);
-let newArr = [];
-for (let i of arr ) {
-    //console.log(i);
-    //timeout += i;
-    //console.log(process.argv);
-    //console.log(arr);
-    console.log(i);
+let args=process.argv.slice(2);
+//let newArr = [];
+for (let arg of args ) {
+  const timeout = arg*1000;
+  if( timeout && timeout > 0 ){ 
     setTimeout(() => {
       process.stdout.write('\x07');
-    }, i);
-  // if ( i === Number) {
-  //   //beep=process.argv[i];
-  //   setTimeout(() => {
-  //     process.stdout.write('\x07');
-  //   }, i);
-  // }
+      console.log("beep",timeout); 
+    }, timeout);
+  } 
 }
+ 
+// // console.log(process.argv.sort((a,b)=>{return a-b}));
+// // console.log(process.argv.length-2);
+// //let arr=(process.argv).slice(2);
+
+//   // for (let i=2; i < process.argv.length-1; i ++ ) {
+//   //   //console.log(i);
+//   //   //timeout += i;
+//   //   if (process.argv[i] === Number) {
+//   //     beep=process.argv[i];
+//   //     setTimeout(() => {
+//   //     process.stdout.write('\x07');
+//   //     }, i );
+//   //   }
+//   //   // }else if( process.argv[i] < 0 || i !== Number){
+//   //   //   break;
+//   //   // }else if (i < 0 || i !== Number ){
+//   //   //    break;
+//   //   // }else{
+//   //   //   break;
+//   //   // }
+//   // }
+// let arr=process.argv.slice(2);
+// let newArr = [];
+// for (let i of arr ) {
+//     //console.log(i);
+//     //timeout += i;
+//     //console.log(process.argv);
+//     //console.log(arr);
+//     console.log(i);
+//     setTimeout(() => {
+//       process.stdout.write('\x07');
+//     }, i);
+//   // if ( i === Number) {
+//   //   //beep=process.argv[i];
+//   //   setTimeout(() => {
+//   //     process.stdout.write('\x07');
+//   //   }, i);
+//   // }
+// }
